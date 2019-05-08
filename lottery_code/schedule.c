3a2af59be15e14104207f3dcd7633d9c7bf5e57b
Gustavo Collaço <37025851+gucollaco@users.noreply.github.com>
2019-05-07 23:13:18 -0300

    Some more compilation errors fixed

--- lottery_code/schedule.c
+++ lottery_code/schedule.c
@@ -123,7 +123,8 @@ int do_noquantum(message *m_ptr)
 		rmp->priority += 1; /* lower priority */
 	}
 
-	struct schedproc *proc_winner = do_lottery(); /* MODIFICADO: vamos obter o processo vencedor, antes de realizar o escalonamento */
+	struct schedproc *proc_winner;
+	proc_winner = do_lottery(); /* MODIFICADO: vamos obter o processo vencedor, antes de realizar o escalonamento */
 
 	if ((rv = schedule_process_local(proc_winner)) != OK) {
 		return rv;
@@ -254,7 +255,8 @@ int do_start_scheduling(message *m_ptr)
 
 	/* Schedule the process, giving it some quantum */
 	pick_cpu(rmp);
-	struct schedproc *proc_winner = do_lottery(); /* MODIFICADO: vamos obter o processo vencedor, antes de realizar o escalonamento */
+	struct schedproc *proc_winner;
+	proc_winner = do_lottery(); /* MODIFICADO: vamos obter o processo vencedor, antes de realizar o escalonamento */
 	while ((rv = schedule_process(proc_winner, SCHEDULE_CHANGE_ALL)) == EBADCPU) { /* MODIFICADO: passando o processo vencedor como parametro */
 		/* don't try this CPU ever again */
 		cpu_proc[proc_winner->cpu] = CPU_DEAD; /* MODIFICADO: usando o processo vencedor */
