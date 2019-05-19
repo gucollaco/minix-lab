41083b62ae1e82aca9ba2754a77958d600849cdb
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2019-05-19 03:29:03 -0300

    lets see

--- lottery_code/proc.c
+++ lottery_code/proc.c
@@ -1805,7 +1805,7 @@ static struct proc * pick_proc(void)
   struct proc **rdy_head;
   int q;				/* iterate over queues */
 
-  register struct proc *aux; /* MODIFICADO: ponteiro que ira percorrer as filas */
+  struct proc *aux; /* MODIFICADO: ponteiro que ira percorrer as filas */
   int total_tickets = 0; /* MODIFICADO: variavel para guardar o total de tickets */
 
   /* MODIFICADO: percorrer todos os processos, atribuindo os tickets e somando a variavel que acumula o total */
@@ -1837,11 +1837,7 @@ static struct proc * pick_proc(void)
 	return rp;
   }
 
-  /* MODIFICADO: realizacao da loteria */
-  int random = (rando()%total_tickets)+1; /* MODIFICADO: variavel que recebe o ticket sorteado */
-  int winner = 0; /* MODIFICADO: variavel para acumular os tickets, ateh que se encontre o vencedor */
-
-  for (q=7; q <= 15; q++) {	
+  for (q=15; q <= 15; q++) {	
 	if(!(rp = rdy_head[q])) {
 		TRACE(VF_PICKPROC, printf("cpu %d queue %d empty\n", cpuid, q););
 		continue;
@@ -1850,6 +1846,24 @@ static struct proc * pick_proc(void)
 	if (priv(rp)->s_flags & BILLABLE)	 	
 		get_cpulocal_var(bill_ptr) = rp; /* bill for system time */
 	return rp;
+  }
+
+  /* MODIFICADO: realizacao da loteria */
+  int random = (rando()%total_tickets)+1; /* MODIFICADO: variavel que recebe o ticket sorteado */
+  int winner = 0; /* MODIFICADO: variavel para acumular os tickets, ateh que se encontre o vencedor */
+
+  for (q=7; q <= 14; q++) {
+    aux = rdy_head[q];
+    while(aux) {
+        winner += aux->p_tickets;
+        if(winner >= random) {
+            assert(proc_is_runnable(aux));
+	        if (priv(aux)->s_flags & BILLABLE)
+	            get_cpulocal_var(bill_ptr) = aux; /* bill for system time */
+	        return aux;
+        }
+        aux = aux->p_nextready;
+    }
   }
 
   return NULL;
